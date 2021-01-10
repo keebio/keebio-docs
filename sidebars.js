75b4e16ec9e2a54ce922084c8a039445ff5dc46d
Danny <danny@keeb.io>
2021-01-09 21:57:50 -0500

    Expand Additional Info section by default

--- sidebars.js
+++ sidebars.js
@@ -1,37 +1,45 @@
 module.exports = {
-  someSidebar: {
-    'Build Guides': [
-      'index',
-      'quefrency-rev2-sinc-build-guide',
-      'iris-rev3-build-guide',
-      'nyquist-rev3-build-guide',
-      'bdn9-rev1.3-build-guide',
-      'bdn9-rev2-build-guide',
-      'iris-rev2-build-guide',
-      'chocopad-build-guide',
-      {
-        'Archive': [
-          'quefrency-rev1-build-guide',
-          'bdn9-build-guide',
-          'laplace-build-log',
-          'dilly-build-guide',
-          'fourier-build-guide',
-          'iris-choc-build-guide',
-          'iris-led-fix',
-          'nyquist-build-guide'
-        ]
-      }
-    ],
-    'Additional Info': [
-      'soldering-tools',
-      'testing-pcb',
-      'adding-rgb-underglow',
-      'case-files',
-      'flashing-firmware',
-      'schematics',
-      'troubleshooting',
-      'faq',
-      'dfu-on-pro-micro'
-    ]
-  },
+  docs: [
+    {
+      'Build Guides': [
+        'index',
+        'quefrency-rev2-sinc-build-guide',
+        'iris-rev3-build-guide',
+        'nyquist-rev3-build-guide',
+        'bdn9-rev1.3-build-guide',
+        'bdn9-rev2-build-guide',
+        'iris-rev2-build-guide',
+        'chocopad-build-guide',
+        {
+          'Archive': [
+            'quefrency-rev1-build-guide',
+            'bdn9-build-guide',
+            'laplace-build-log',
+            'dilly-build-guide',
+            'fourier-build-guide',
+            'iris-choc-build-guide',
+            'iris-led-fix',
+            'nyquist-build-guide'
+          ]
+        }
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Additional Info',
+      items: [
+        'soldering-tools',
+        'testing-pcb',
+        'lighting-options',
+        'adding-rgb-underglow',
+        'case-files',
+        'flashing-firmware',
+        'schematics',
+        'troubleshooting',
+        'faq',
+        'dfu-on-pro-micro'
+      ],
+      collapsed: false
+    }
+  ]
 };
